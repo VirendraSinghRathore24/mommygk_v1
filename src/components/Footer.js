133fcfdb07256140af0836333e0bda33c7191d69
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-07-11 11:51:34 +0530

    insta commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 function Footer() {
   return (
     <div>
-        <div className=' shadow-md w-full flex flex-col justify-center items-center border-2 py-2  bottom-0'>
+        <div className='shadow-md w-full flex  flex-col justify-center items-center border-2 py-2 bottom-0'>
           <div className='p-5'>
             <p className='text-3xl text-center w-full'>Join Our Community</p>
             <p className='text-xl text-center font-normal p-2'>To stay connected with Sanju and receive updates on the latest post, you can connect on the following platforms</p>
               <div className='flex justify-evenly gap-x-8 py-5'>
-                <Link to="https://www.instagram.com/mumma_and_rudransh/">
+                <Link to="https://www.instagram.com/mommygk_sanju/">
                 <img src='https://res.cloudinary.com/dixqxdivr/image/upload/v1688708911/instagram-1581266_1280_fwpbeq.jpg' alt='instagram' className='w-[100px] h-[100px]'/>
                 </Link>
                 <Link to="https://www.youtube.com/@MommyGK">
