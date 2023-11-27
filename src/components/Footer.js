4e7bc4d69b566e98db27a2475ce61b4ba119f54d
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-11-27 12:15:34 +0530

    footer1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,14 +10,18 @@ function Footer() {
             <p className='text-xl text-center font-normal p-2'>To stay connected with Sanju and receive updates on the latest post, you can connect on the following platforms</p>
               <div className='flex justify-evenly gap-x-8 py-2'>
                 <Link to="https://www.instagram.com/mommy_sanju/">
-                <img src='https://res.cloudinary.com/dixqxdivr/image/upload/v1688708911/instagram-1581266_1280_fwpbeq.jpg' alt='instagram' className='w-[100px] h-[100px]'/>
+                <img src='https://res.cloudinary.com/dixqxdivr/image/upload/v1688708911/instagram-1581266_1280_fwpbeq.jpg' alt='instagram' className='w-[70px] h-[70px] mt-1'/>
                 </Link>
                 <Link to="https://www.youtube.com/@MommyGK">
-                <img src='https://res.cloudinary.com/dixqxdivr/image/upload/v1688707836/youtube-1837872_1280_oicf29.png' alt='youtube' className='w-[70px] h-[70px] mt-4'/>
+                <img src='https://res.cloudinary.com/dixqxdivr/image/upload/v1688707836/youtube-1837872_1280_oicf29.png' alt='youtube' className='w-[50px] h-[50px] mt-4'/>
                 </Link>
                 <Link to="https://www.facebook.com/people/Sanju-Shekhawat/100094344244292/?mibextid=LQQJ4d">
-                <img src='../../images/facebook.png' alt='linkedin' className='w-[70px] h-[70px] mt-4'/>
+                <img src='../../images/facebook.png' alt='linkedin' className='w-[50px] h-[50px] mt-4'/>
                 </Link>
+                <Link to="https://www.linkedin.com/in/sanju-shekhawat-59344b44/">
+                <img src='https://res.cloudinary.com/dixqxdivr/image/upload/v1686808399/Viren/linkedin.png.png' alt='linkedin' className='w-[50px] h-[50px] mt-4'/>
+                </Link>
+                
               </div>
               <p className='text-center'>Copyright © 2023 Mommy GK. All rights reserved.</p>
           </div>
