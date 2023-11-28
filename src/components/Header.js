e8660801d8fd3bfbb65a335638e6c043ecb69251
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-11-28 16:37:14 +0530

    latest1

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,9 +46,6 @@ function Header() {
                 <li><a className="py-5 flase text-lg leading-5 relative group flex gap-1 items-center" href="/babyfood6month">
                 <span>Downloads</span>
                 </a></li>
-                <li><a className="py-5 flase text-lg leading-5 relative group flex gap-1 items-center" href="/users1">
-                <span>Users</span>
-                </a></li>
                 <li><a className="py-5 flase text-lg leading-5 relative group flex gap-1 items-center" href="/aboutus">
                 <span>About us</span>
                 </a></li>         
@@ -80,7 +77,6 @@ function Header() {
         <li className="text-center"><a class=" text-xl leading-5" href="/">Home</a></li>
         <li className="text-center"><a class=" text-xl leading-5" href="/food">Food</a></li>
         <li className="text-center"><a class="text-xl leading-5" href="/babyfood6month">Downloads</a></li>
-        <li className="text-center"><a class="text-xl leading-5" href="/users1">Users</a></li>
         <li className="text-center"><a class=" text-xl leading-5" href="/aboutus">About us</a></li>
         </ul>
        </div>
