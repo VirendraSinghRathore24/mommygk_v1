18d2ccfcebd7f56df52bc394915045a70150f42f
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-12-01 15:43:52 +0530

    ad

--- src/App.js
+++ src/App.js
@@ -53,7 +53,9 @@ export default function App() {
           <Route path="/termsofuse" element={<TermsOfUse/>}/>
       </Routes>
       <Footer/>
-      <PageComponent />
+      <>      <PageComponent />
+      </>
+
     </div>
   )
 }
--- src/components/PageComponent.js
+++ src/components/PageComponent.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React, { useEffect } from 'react';
 
-const PageComponent = () => {
-  return (
-    <div>
-      {/* Your page content goes here */}
-      <Helmet>
-        {/* Add the Google AdSense script to the head */}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <script>
-          {`(adsbygoogle = window.adsbygoogle || []).push(
-            {
-                google_ad_client: ca-pub-2373979360301400,
-                enable_page_level_ads: true
-            });`}
-        </script>
-      </Helmet>
-    </div>
-  );
+const PageComponent = (props) => {
+    const { dataAdSlot } = props;  
+    useEffect(() => {
+
+        try {
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+        }
+
+        catch (e) {
+
+        }
+
+    },[]);
+
+    return (
+        <>
+            <ins className="adsbygoogle"
+                style={{ display: "block" }}
+                data-ad-client="ca-pub-2373979360301400"
+                data-ad-slot={dataAdSlot}
+                data-ad-format="auto"
+                data-full-width-responsive="true">
+            </ins>
+        </>
+    );
 };
 export default PageComponent;
