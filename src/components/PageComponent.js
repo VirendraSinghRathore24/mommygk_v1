9b0de660f67abf4636b89fdc0f963dc7604d6a04
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-12-01 16:23:08 +0530

    ad1

--- src/components/PageComponent.js
+++ src/components/PageComponent.js
@@ -1,29 +1,23 @@
 import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet-async';
 
-const PageComponent = (props) => {
-    const { dataAdSlot } = props;  
-    useEffect(() => {
 
-        try {
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-        }
-
-        catch (e) {
-
-        }
-
-    },[]);
+const PageComponent = () => {
 
     return (
-        <>
-            <ins className="adsbygoogle"
-                style={{ display: "block" }}
-                data-ad-client="ca-pub-2373979360301400"
-                data-ad-slot={dataAdSlot}
-                data-ad-format="auto"
-                data-full-width-responsive="true">
-            </ins>
-        </>
+        <div>
+            <Helmet>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <script>
+                    {
+                        `(adsbygoogle=window.adsbygoogle || []).push({
+                            google_ad_client:'ca-pub-2373979360301400',
+                            enable_page_level_ads: true
+                        });`
+                    }
+                </script>
+            </Helmet>
+        </div>
     );
 };
 export default PageComponent;
