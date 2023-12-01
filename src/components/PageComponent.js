d91224542a58c2b8bd7e66426ec09014654d6f9d
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-12-01 12:10:06 +0530

    ads111

--- src/components/PageComponent.js
+++ src/components/PageComponent.js
@@ -7,8 +7,14 @@ const PageComponent = () => {
       {/* Your page content goes here */}
       <Helmet>
         {/* Add the Google AdSense script to the head */}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4401844428563263"
-     crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+          {`(adsbygoogle = window.adsbygoogle || []).push(
+            {
+                google_ad_client: ca-pub-4401844428563263,
+                enable_page_level_ads: true
+            });`}
+        </script>
       </Helmet>
     </div>
   );
