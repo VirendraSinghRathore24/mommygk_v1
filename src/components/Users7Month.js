7065c3eee51326f3db075da5c3f070d2f457b0c9
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-12-04 13:36:37 +0530

    align1

--- src/components/Users7Month.js
+++ src/components/Users7Month.js
@@ -36,11 +36,11 @@ function Users7Month() {
                     <table className="min-w-full text-left text-sm font-light">
                     <thead className="border-b font-medium dark:border-neutral-500">
                         <tr>
-                        <th scope="col" className="px-1 py-2">#</th>
-                        <th scope="col" className="px-4 py-2">City</th>
-                        <th scope="col" className="px-4 py-2">State</th>
-                        <th scope="col" className="px-2 py-2">Country</th>    
-                        <th scope="col" className="px-2 py-2">Count</th>                     
+                        <th scope="col" className="px-1 py-2 border-r-2 text-center">#</th>
+                        <th scope="col" className="px-4 py-2 border-r-2 text-center">City</th>
+                        <th scope="col" className="px-4 py-2 border-r-2 text-center">State</th>
+                        <th scope="col" className="px-2 py-2 border-r-2 text-center">Country</th>    
+                        <th scope="col" className="px-2 py-2 text-center">Count</th>                     
                         </tr>
                     </thead>
             <tbody>
@@ -49,11 +49,11 @@ function Users7Month() {
                         
                             <tr className="border-b dark:border-neutral-500">
                                 
-                                <td className="whitespace-nowrap px-1 py-2 font-medium">{index + 1} .</td>
-                                <td className="whitespace-wrap text-[16px] font-medium px-1 py-2">{post.city}</td>
-                                <td className="whitespace-wrap text-[16px] font-medium px-1 py-2">{post.state}</td>
-                                <td className="whitespace-wrap text-[16px] font-medium px-4 py-2">{post.country}</td>
-                                <td className="whitespace-wrap text-[16px] font-medium px-4 py-2">{post.count}</td>
+                                <td className="whitespace-nowrap px-1 py-2 font-medium border-r-2 text-center">{index + 1} .</td>
+                                <td className="whitespace-wrap text-[16px] font-medium px-1 py-2 border-r-2 text-center">{post.city}</td>
+                                <td className="whitespace-wrap text-[16px] font-medium px-1 py-2 border-r-2 text-center">{post.state === 'National Capital Territory of Delhi' ? 'Delhi' : post.state}</td>
+                                <td className="whitespace-wrap text-[16px] font-medium px-4 py-2 border-r-2 text-center">{post.country}</td>
+                                <td className="whitespace-wrap text-[16px] font-medium px-4 py-2 text-center">{post.count}</td>
                                 
                             </tr>
                     ))
