ef90ed3c8d5bab4a687d7cd1063abf219309134c
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-12-06 15:05:47 +0530

    about

--- src/components/BabyFood.js
+++ src/components/BabyFood.js
@@ -2,8 +2,12 @@ import React from 'react';
 import axios from "axios";
 import { useEffect, useState } from "react";
 import baseUrl from '../baseUrl';
+import data1 from '../data/food6month.json'
+import FoodMonthsCard from './FoodMonthsCard';
 
 function BabyFood() {
+    const [posts, setPosts] = useState([]);
+
   const getGeoInfo = async () => {
     const data = new FormData();
     axios.get('https://ipapi.co/json/').then((response) => {
@@ -19,9 +23,23 @@ function BabyFood() {
         console.log(error);
     });     
 };
+ function fetchBlogsData(){
+    //setLoading(true);
+    try{
+        console.log("Viren");
+        console.log(data1[0].details);
+        setPosts(data1);
+
+    }
+    catch(err){
+      console.log(err);
+    }
+    //setLoading(false);
+  }
 
   useEffect(() => {
     getGeoInfo();
+    fetchBlogsData();
     window.scroll(0,0);
   }, []);
 
@@ -43,19 +61,30 @@ function BabyFood() {
         </div>
         </div>
                 <div>
-                <div className='p-4 flex items-center justify-center'>
+              
+                    <div className='flex text-center items-center justify-center bg-red-300 h-8 gap-y-1 text-blue-600 font-semibold'>
+                    <marquee>
+                        Currently YouTube and Blog link will not work as we are working on it. You can subscribe for updates...
+                        </marquee>
+                    </div>
+
+                <div className='mt-4 flex items-center justify-center p-2'>
                      <img src="../../images/All1.png" loading='lazy' className=' rounded-md'/>
                 </div>
-                    <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 px-10 py-5 gap-x-10 gap-y-10'>
+                <div className='grid xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-4 px-4 py-1 gap-x-10 gap-y-1'>
+                {
+                posts.map((post, index) => (
+                    <FoodMonthsCard key={index} post={post.details} week={post.week} time={post.time} color={post.color}/>
+                ))
+                }
+                </div>
+                    {/* <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 px-10 py-5 gap-x-10 gap-y-10'>
                             <img src="../../images/Week1.png" loading='lazy' className=' rounded-md'/>
                             <img src="../../images/Week2.png" loading='lazy' className=' rounded-md'/>
                             <img src="../../images/Week3.png" loading='lazy' className=' rounded-md'/>
                             <img src="../../images/Week4.png" loading='lazy' className=' rounded-md'/>
                         
-                    </div>
-                </div>
-                <div className=' mb-4 ml-10 font-semibold'>
-                Please find the reciepe videos in my Instagram and YouTube channel.
+                    </div> */}
                 </div>
     </div>
   )
---
+++ src/components/FoodMonthsCard.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+function FoodMonthsCard({post, week, time, color}) {
+  return (
+    <div>
+    <div >
+    <p className='p-2 text-[16px]'></p>
+        <div className='flex flex-col gap-y-2 ml-1 mt-6 font-semibold'>
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            {
+                color === 1 ? (<div className='inline-block min-w-full text-center py-2 h-14 bg-lime-300 border-2 border-black'>{week}</div>) :
+                color === 2 ? (<div className='inline-block min-w-full text-center py-2 h-14 bg-yellow-400 border-2 border-black'>{week}</div>) :
+                color === 3 ? (<div className='inline-block min-w-full text-center py-2 h-14 bg-red-300 border-2 border-black'>{week}</div>) :
+                              (<div className='inline-block min-w-full text-center py-2 h-14 bg-sky-500 border-2 border-black'>{week}</div>)
+            }
+            
+            <div className="overflow-hidden">
+                <table className="min-w-full text-left text-sm font-light">
+                <thead className=" font-medium ">
+                    <tr className='bg-orange-300 border-2 border-black'>
+                    <th scope="col" className="px-1 py-2 border-r-2 text-center">Day</th>
+                    <th scope="col" className="px-1 py-2 border-r-2 text-center">{time}</th>
+                    <th scope="col" className="px-6 py-2 border-r-2 text-center" >Video</th>
+                    <th scope="col" className="px-6 py-2 text-center ">Blog</th>
+                    
+                    </tr>
+                </thead>
+        <tbody>
+        {
+            post.map((p, index) => (
+                        <tr className=" border-2 border-black">
+                            <td className="whitespace-wrap text-[14px] font-medium px-1 py-2 border-r-2 text-center">{p.day}</td>
+                            <td className="whitespace-wrap text-[14px] font-medium px-1 py-2 border-r-2 text-center text-blue-600 underline">
+                                <a href={p.youtubelink} target="_blank" rel="noreferrer">
+                                    {p.Reciepe1}
+                                </a>
+                            </td>
+                            <td className='px-6 py-2 w-[20px] h-[20px] border-r-2 text-center'> 
+                                <a href={p.youtubelink} target="_blank" rel="noreferrer">
+                                <img src="../../images/youtube.png" className='w-[35px] h-[35px] rounded-md' loading='lazy'/>
+                                </a>
+                            </td>
+                            <td className='px-6 py-2 w-[40px] h-[40px] text-center'> 
+                               
+                                <img src="../../images/article.png" className='w-[35px] h-[35px] rounded-md' loading='lazy'/>
+                                
+                            </td>
+                            
+                        </tr>     
+            ))      
+           } 
+           </tbody>
+           </table>
+        </div>
+        </div>
+        </div>
+        </div>
+    </div></div>
+  )
+}
+
+export default FoodMonthsCard
