17a83a4e23b54d56af03d490a8304fc2812d1bc2
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-12-22 21:10:37 +0530

    aa

--- src/components/FoodReciepesVideo.js
+++ src/components/FoodReciepesVideo.js
@@ -10,7 +10,7 @@ function FoodReciepesVideo() {
   return (
     <div>
         <p className='text-md font-light py-3 px-7'><strong className='font-semibold text-xl text-richblack-700'>Food Recipes Video :</strong> </p>
-            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 px-7 py-5 gap-x-10 gap-y-10 '>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 px-9 py-5 gap-x-10 gap-y-10 '>
             {
                 
                 posts.map((post, index) => (
