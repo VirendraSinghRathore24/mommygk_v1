1e3fbf78163ad8a22023369bc408d5cf4fa4d168
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-12-22 21:15:24 +0530

    aa

--- src/components/FoodVideoCard.js
+++ src/components/FoodVideoCard.js
@@ -9,7 +9,7 @@ function FoodVideoCard({post}) {
     <div>
          <div className='relative pointer-events-auto w-[300px] h-[300px] mb-10'>
          <Link to={`${post.youtubelink}`} target="_blank">
-                <img src={post.imageUrl} loading='lazy' className='w-[300px] h-[300px] rounded-md'/>
+                <img src={post.imageUrl} loading='lazy' className='w-[320px] h-[320px] rounded-md'/>
                 <p className='text-[20px] text-center text-richblack-700 italic'>{post.title}</p>
                 <div className='absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                     
