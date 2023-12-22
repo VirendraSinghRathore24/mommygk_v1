60a1ed2bcfc24edaadafdc7558d6941b16b9f573
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2023-12-22 20:43:21 +0530

    aa

--- src/components/FoodVideoCard.js
+++ src/components/FoodVideoCard.js
@@ -4,12 +4,12 @@ import { CiPlay1 } from "react-icons/ci";
 
 
 function FoodVideoCard({post}) {
-    console.log(post);
+    
   return (
     <div>
-         <div className='relative pointer-events-auto '>
+         <div className='relative pointer-events-auto w-[300px] h-[300px] mb-10'>
          <Link to={`${post.youtubelink}`} target="_blank">
-                <img src={post.imageUrl} loading='lazy' className='w-[380px] h-[300px] rounded-md'/>
+                <img src={post.imageUrl} loading='lazy' className='w-[300px] h-[300px] rounded-md'/>
                 <p className='text-[20px] text-center text-richblack-700 italic'>{post.title}</p>
                 <div className='absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                     
