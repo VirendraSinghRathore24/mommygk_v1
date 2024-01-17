04dc4bac892e9e6e1b8fdf8d48aa36ea776b0939
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-01-17 10:29:06 +0530

    update1

--- src/components/PageComponent.js
+++ src/components/PageComponent.js
@@ -12,9 +12,8 @@ const PageComponent = () => {
                 <script>
                     {
                         `(adsbygoogle=window.adsbygoogle || []).push({
-                            google_ad_client:'ca-pub-4401844428563263',
-                            enable_page_level_ads: true,
-                            crossorigin : 'anonymous'
+                            google_ad_client:'ca-pub-1540353019718575',
+                            enable_page_level_ads: true
                         });`
                     }
                 </script>
