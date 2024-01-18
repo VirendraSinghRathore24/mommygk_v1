e814a10d89be868ac17d5e5a9d67d7878c152fed
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-01-18 16:49:36 +0530

    update1

--- src/components/FoodVideoCard.js
+++ src/components/FoodVideoCard.js
@@ -21,26 +21,49 @@ function FoodVideoCard({post}) {
     //         </div>  
             
     // </div>
-    <div className='w-[310px] bg-bgDark bg-opacity-80 rounded-lg overflow-hidden shadow-lg'>
-     <Link to={`${post.youtubelink}`} target="_blank">
-        <div className='relative pointer-events-auto'>
-            <img className='' src={post.imageUrl} loading='lazy' ></img>
+    // <div className='w-[310px] bg-bgDark bg-opacity-80 rounded-lg overflow-hidden shadow-lg'>
+    //  <Link to={`${post.youtubelink}`} target="_blank">
+    //     <div className='relative pointer-events-auto'>
+    //         <img className='' src={post.imageUrl} loading='lazy' ></img>
 
+    //         <div className='absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                    
+    //                  <IoPlayCircleOutline size={100} />
+                    
+                    
+    //            </div>
+    //     </div>
+    //     <div className='p-4'>
+    //         <p className='text-blue-500 font-bold text-xl leading-6'>{post.title}</p>
+    //         <p className=' mt-2'>
+    //         {
+    //             post.shortDesc
+    //         }
+    //         </p>
+    //     </div>
+    //     </Link>
+    // </div>
+    <div className='relative w-[330px] bg-bgDark bg-opacity-80 rounded-lg overflow-hidden shadow-lg pointer-events-auto '>
+   <Link to={`${post.youtubelink}`} target="_blank">
+        
+            <img className='' src={post.imageUrl} loading='lazy' ></img>
             <div className='absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                     
-                     <IoPlayCircleOutline size={100} />
+                   <IoPlayCircleOutline size={100} />
                     
                     
-               </div>
+              </div>
+            {/* <div>
+                <button className='w-[100px] h-[40px] text-white bg-pink-500 rounded-lg absolute right-2 bottom-[-12px] grid place-items-center font-bold  hover:scale-110 transition duration-300 ease-in'>Explore</button>
+            </div> */}
+       <div>
+       <div className='absolute text-white bottom-4 px-4 text-left text-wrap bg-white h-20 w-full'>
+            <p className='text-black font-bold text-xl leading-6 text-center mt-6'>{post.title}</p>
         </div>
-        <div className='p-4'>
-            <p className='text-blue-500 font-bold text-xl leading-6'>{post.title}</p>
-            <p className=' mt-2'>
-            {
-                post.shortDesc
-            }
-            </p>
+      
+       
         </div>
+        
         </Link>
     </div>
   )
