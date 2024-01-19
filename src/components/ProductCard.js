93d403ff04e77b7258fe2518fc714529b24ad68e
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-01-19 22:55:45 +0530

    amazon

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,7 +5,7 @@ function ProductCard({url, imageUrl}) {
   return (
     <div className='relative  bg-bgDark bg-opacity-80 rounded-lg overflow-hidden shadow-lg pointer-events-auto'>
         <NavLink to={`${url}`} target="_blank">
-            <img className='w-[250px] h-[250px]' src={imageUrl} loading='lazy' ></img>
+            <img className='' src={imageUrl} loading='lazy' ></img>
         </NavLink>
     </div>
   )
--- src/components/Toys.js
+++ src/components/Toys.js
@@ -6,15 +6,34 @@ function Toys() {
         window.scroll(0,0);
       }, []);
   return (
-    <div className='w-10/12 mx-auto text-lg p-8 mt-4  '>
-        <div className='text-3xl uppercase text-red-700 font-mono font-bold '>Baby Toys by age</div>
+    <div className='w-10/12 mx-auto text-lg p-8 mt-4 '>
+    <div className='text-3xl uppercase text-red-700 font-mono font-bold mb-8'>Baby Toys By Age</div>
+        <div className='text-2xl uppercase text-red-700 font-mono font-bold '>6-12 Months</div>
             <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 3xl:grid-cols-4 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
-                <ProductCard url={'https://www.amazon.in/shop/mommygk_sanju/list/1254Y8II0H5FN?tag=onamzsanjuk03-21&ref_=aip_sf_list_spv_ofs_mixed_d'} imageUrl={'../../images/toys6to12.png'}/>
-                <ProductCard url={'https://www.amazon.in/shop/mommygk_sanju/list/27UZI465HMI3B?tag=onamzsanjuk03-21&ref_=aip_sf_list_spv_ofs_mixed_d'} imageUrl={'../../images/12monthstoys.png'}/>
-                <ProductCard url={'https://www.amazon.in/shop/mommygk_sanju/list/2XZWEA1DZJVI8?tag=onamzsanjuk03-21&ref_=aip_sf_list_spv_ofs_mixed_d'} imageUrl={'../../images/halfyeartoys.png'}/>
-                <ProductCard url={'https://www.amazon.in/shop/mommygk_sanju/list/STXIA7YLY14N?tag=onamzsanjuk03-21&ref_=aip_sf_list_spv_ofs_mixed_d'} imageUrl={'../../images/1yeartoys.png'}/>
-                <ProductCard url={'https://www.amazon.in/shop/mommygk_sanju/list/3JCW80G6YQQ0B?tag=onamzsanjuk03-21&ref_=aip_sf_list_spv_ofs_mixed_d'} imageUrl={'../../images/panda.png'}/>
-                <ProductCard url={'https://www.amazon.in/shop/mommygk_sanju/list/3SKMXHQGH0NT?tag=onamzsanjuk03-21&ref_=aip_sf_list_spv_ofs_mixed_d'} imageUrl={'../../images/books.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B081VM89M1?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B081VM89M1&asc_item-id=amzn1.ideas.1254Y8II0H5FN&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/6month1.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B09N3K9V44?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B09N3K9V44&asc_item-id=amzn1.ideas.1254Y8II0H5FN&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/6month2.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B0B49L799W?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B0B49L799W&asc_item-id=amzn1.ideas.1254Y8II0H5FN&ref_=aip_sf_list_spv_ofs_mixed_d_asin&th=1'} imageUrl={'../../images/6month3.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B077GQ9NW2?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B077GQ9NW2&asc_item-id=amzn1.ideas.1254Y8II0H5FN&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/6month4.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B074V7RRTG?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B074V7RRTG&asc_item-id=amzn1.ideas.1254Y8II0H5FN&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/6month5.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B01MSC4TXQ?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B01MSC4TXQ&asc_item-id=amzn1.ideas.1254Y8II0H5FN&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/6month6.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B0BS9BM9MH?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B0BS9BM9MH&asc_item-id=amzn1.ideas.1254Y8II0H5FN&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/6month7.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B00ADZULW4?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B00ADZULW4&asc_item-id=amzn1.ideas.1254Y8II0H5FN&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/6month8.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B00ADZUNYK?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B00ADZUNYK&asc_item-id=amzn1.ideas.1254Y8II0H5FN&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/6month9.png'}/>
+            </div>
+            <div className='text-2xl uppercase text-red-700 font-mono font-bold '>12+ Months</div>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 3xl:grid-cols-4 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
+                <ProductCard url={'https://www.amazon.in/dp/B0B975J4GT?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B0B975J4GT&asc_item-id=amzn1.ideas.27UZI465HMI3B&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/12months1.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B09WYF85K1?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B09WYF85K1&asc_item-id=amzn1.ideas.27UZI465HMI3B&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/12months2.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B09BJ91Z2N?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B09BJ91Z2N&asc_item-id=amzn1.ideas.STXIA7YLY14N&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/12months3.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B09CTRPSJR?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B09CTRPSJR&asc_item-id=amzn1.ideas.STXIA7YLY14N&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/12months4.png'}/>
+            </div>
+            <div className='text-2xl uppercase text-red-700 font-mono font-bold '>18+ Months</div>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 3xl:grid-cols-4 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
+                <ProductCard url={'https://www.amazon.in/dp/B093GTTG3B?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B093GTTG3B&asc_item-id=amzn1.ideas.2XZWEA1DZJVI8&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/15months1.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B0BW7PBS92?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B0BW7PBS92&asc_item-id=amzn1.ideas.2XZWEA1DZJVI8&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/15months2.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B07SYJXQTR?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B07SYJXQTR&asc_item-id=amzn1.ideas.2XZWEA1DZJVI8&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/15months3.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B0B975J4GT?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B0B975J4GT&asc_item-id=amzn1.ideas.27UZI465HMI3B&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/12months1.png'}/>
+                <ProductCard url={'https://www.amazon.in/dp/B0B19TDXBX?linkCode=ssc&tag=onamzsanjuk03-21&creativeASIN=B0B19TDXBX&asc_item-id=amzn1.ideas.2XZWEA1DZJVI8&ref_=aip_sf_list_spv_ofs_mixed_d_asin'} imageUrl={'../../images/15months4.png'}/>
             </div>
   </div>
   )
