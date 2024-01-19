ceb3b345b109cfabdc05618d4c11eb5b23a9cc08
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-01-20 00:06:25 +0530

    amazon

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -63,17 +63,16 @@ function HomePage() {
           </div>
           <div className='w-10/12 mx-auto text-lg p-8 '>
             <div className='xs:text-xl sm:text-xl md:text-3xl uppercase text-red-700 font-mono font-bold'>Baby Toys Checklist</div>
-            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 3xl:grid-cols-4 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-3 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
             <ProductCard url={'/toys'} imageUrl={'../../images/toys6to12.png'}/>
             <ProductCard url={'/toys'} imageUrl={'../../images/halfyeartoys.png'}/>
             <ProductCard url={'/toys'} imageUrl={'../../images/12monthstoys.png'}/>
-            <ProductCard url={'/toys'} imageUrl={'../../images/1yeartoys.png'}/>
             </div>
             <div className='text-red-700 font-mono font-bold'>See More <span className='text-blue-600 underline'><Link to={'/toys'}>Baby Toys</Link></span></div>
           </div>
           <div className='w-10/12 mx-auto text-lg p-8'>
             <div className='xs:text-xl sm:text-xl md:text-3xl uppercase text-red-700 font-mono font-bold '>Baby Care Products Checklist</div>
-            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 3xl:grid-cols-4 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-3 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
             <ProductCard url={'/newmomchecklist'} imageUrl={'../../images/newmomchecklist.png'}/>
             <ProductCard url={'/foodplatecheklist'} imageUrl={'../../images/foodplate.png'}/>
             <ProductCard url={'/proofingheklist'} imageUrl={'../../images/babyproofing.png'}/>
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,7 +5,7 @@ function ProductCard({url, imageUrl}) {
   return (
     <div className='relative  bg-bgDark bg-opacity-80 rounded-lg overflow-hidden shadow-lg pointer-events-auto'>
         <NavLink to={`${url}`} >
-            <img className='' src={imageUrl} loading='lazy' ></img>
+            <img className='w-[250px] h-[250px]' src={imageUrl} loading='lazy' ></img>
         </NavLink>
     </div>
   )
