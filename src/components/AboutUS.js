6f0234a150d52becb5aeb645972586891eb64d41
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-01-22 14:30:00 +0530

    about us

--- src/components/AboutUS.js
+++ src/components/AboutUS.js
@@ -13,12 +13,12 @@ function AboutUS() {
     {/* <p className='ml-10 text-3xl text-blue-400'>About Me</p> */}
     <div className='flex flex-wrap w-full justify-evenly gap-y-2'>
         <div className='flex flex-col lg:w-8/12 xs:w-screen items-center font-normal p-5'>
-            <p className='leading-7 text-md align-baseline text-justify font-mono'>Hello!!! Thanks for visiting my space and taking the time to read about me. I am Sanju from Rajasthan (India) and I live in Bangalore. I am working in IT industry as a software engineer for 10 years. I am mother of Rudransh Rathore and during this time I found my passion! Yes it's cooking and clicking, and so this blog!
+            <p className='leading-8 text-md align-baseline text-justify font-mono'>Hello!!! Thanks for visiting my space and taking the time to read about me. I am Sanju from Rajasthan (India) and I live in Bangalore. I am working in IT industry as a software engineer for 10 years. I am mother of Rudransh Rathore and during this time I found my passion! Yes it's cooking and clicking, and so this blog!
  
  Though I am fond of food and like to taste different varieties, but started my own cooking after my marriage as I never entered into the kitchen because of my lovable mom and only had the opportunity to enjoy her yummy food. But after marriage, I started cooking vigorously. My husband is a best critic, and that helps me to improve my cooking.</p>
         
         <br/>
-        <p className='leading-7 text-md align-baseline text-justify font-mono'>The recipes posted here are tried and tasted in my kitchen,traditional native recipes from my mom, baby foods which I feed my baby and few other recipes tried from friends, cookbooks or other food blogs.Being a health-conscious person, most of my recipes are focuses on health. (Some are exceptional though)</p>
+        <p className='leading-8 text-md align-baseline text-justify font-mono'>The recipes posted here are tried and tasted in my kitchen, traditional native recipes from my mom, baby foods which I feed my baby and few other recipes tried from friends, cookbooks or other food blogs.Being a health-conscious person, most of my recipes are focuses on health. (Some are exceptional though)</p>
         </div>
 
         <div className='p-5'>
