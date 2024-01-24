69fda1ebb757d48c074a205e4529dbb2ef33ee59
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-01-24 20:15:53 +0530

    revert homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -50,7 +50,7 @@ function HomePage() {
     <div className='bg-gradient-to-r from-stone-50 to-blue-50 mt-4 py-8'>
     <div className='w-10/12 mx-auto'>
               <div className='text-3xl text-red-700 uppercase font-mono font-bold '>Popular Post</div>
-              <div className='flex flex-wrap gap-y-4 justify-evenly mt-8'>
+              <div className='grid xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-3 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
               <PopularCard url={'/babyfood6month'} imageUrl={'../../images/foodcharts.JPEG'} title={'6 Month Food Chart'} color={1}/>
               <PopularCard url={'https://www.youtube.com/watch?v=1h-Buyvan-M&t=91s'} imageUrl={'../../images/solidfood2.jpg'} title={'Solid Food Introduction'} color={2}/>
               <PopularCard url={'https://www.youtube.com/watch?v=4S02boyMtWA'} imageUrl={'../../images/carrot2.jpeg'} title={'Carrot Recipe Video'} color={3}/>
@@ -60,7 +60,7 @@ function HomePage() {
           </div>
           <div className='w-10/12 mx-auto text-lg py-8 '>
             <div className='xs:text-xl sm:text-xl md:text-3xl uppercase text-red-700 font-mono font-bold'>Baby Toys Checklist</div>
-            <div className='flex flex-wrap gap-y-4 justify-evenly mt-8'>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 3xl:grid-cols-4 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
             <ProductCard url={'/toys'} imageUrl={'../../images/toys6to12.png'}/>
             <ProductCard url={'/toys'} imageUrl={'../../images/halfyeartoys.png'}/>
             <ProductCard url={'/toys'} imageUrl={'../../images/12monthstoys.png'}/>
@@ -71,7 +71,7 @@ function HomePage() {
           <div className='bg-gradient-to-r from-stone-50 to-blue-50'>
           <div className='w-10/12 mx-auto text-lg py-8'>
             <div className='xs:text-xl sm:text-xl md:text-3xl uppercase text-red-700 font-mono font-bold '>Baby Care Products Checklist</div>
-            <div className='flex flex-wrap gap-y-4 justify-evenly mt-8'>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 3xl:grid-cols-4 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
             <ProductCard url={'/newmomchecklist'} imageUrl={'../../images/newmomchecklist.png'}/>
             <ProductCard url={'/foodplatecheklist'} imageUrl={'../../images/foodplate.png'}/>
             <ProductCard url={'/proofingheklist'} imageUrl={'../../images/babyproofing.png'}/>
@@ -82,7 +82,7 @@ function HomePage() {
           
           <div className='w-10/12 mx-auto text-lg py-8'>
             <div className='xs:text-xl sm:text-xl md:text-3xl uppercase text-red-700 font-mono font-bold '>Our Recent Content at Instagram</div>
-            <div className='flex flex-wrap gap-y-4 justify-evenly mt-8'>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 3xl:grid-cols-4 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
               <div ><InstagramEmbed url="https://www.instagram.com/p/C1bO42oS8I9/" /></div>
               <div ><InstagramEmbed url="https://www.instagram.com/p/CurOGQBSHST/?img_index=1" /></div>
               <div ><InstagramEmbed url="https://www.instagram.com/p/CxqSr6-yGzD/?img_index=1" /></div> 
