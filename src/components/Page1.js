2ffaecb6e45d63768ea0cf1210112da059d91403
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-01-24 20:28:16 +0530

    test

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -40,9 +40,9 @@ function Page1() {
         
         <div className=''>
             <div>
-            <marquee>
-                <h1 className='text-md font-light py-3 px-2'><strong className='text-xl font-extrabold googlefont text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-red-700 to-lime-800'>There is no better feeling in the world than holding your precious child.</strong></h1>
-            </marquee>
+            
+                <p className='text-md font-light py-3 px-2'><strong className='text-xl font-extrabold googlefont text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-red-700 to-lime-800'>There is no better feeling in the world than holding your precious child.</strong></p>
+            
             </div>
             <div className='grid xxs:grid-cols-1 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 3xl:grid-cols-3 place-items-center gap-y-10 gap-x-4 mb-10 mt-4'>
             {
