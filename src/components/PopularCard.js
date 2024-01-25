b3049fdd8ed5cb8da9681599f4f37182e3e1f1b7
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-01-25 14:25:42 +0530

    popular post

--- src/components/PopularCard.js
+++ src/components/PopularCard.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom'
 
 function PopularCard({url, imageUrl, title, color}) {
   return (
-    <div className='relative w-[330px] bg-bgDark bg-opacity-80 rounded-lg overflow-hidden shadow-lg pointer-events-auto'>
+    <div className='relative w-[280px] bg-bgDark bg-opacity-80 rounded-lg overflow-hidden shadow-lg pointer-events-auto'>
     <NavLink to={`${url}`} target='_blank'>
         
-       <img className='h-[350px]' src={imageUrl} loading='lazy' ></img>
+       <img className='h-[300px]' src={imageUrl} loading='lazy' ></img>
        <div className='font-mono hover:scale-110 transition duration-300 ease-in'>
        {
         color === 1 ?  (<div className='absolute text-white bottom-4 px-4 text-left text-wrap bg-stone-300 h-20 w-full'>
