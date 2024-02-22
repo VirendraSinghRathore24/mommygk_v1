50ec15825bafc8579e093a1d237b151022d625cd
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-02-22 15:06:14 +0530

    insta update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -48,7 +48,7 @@ function HomePage() {
           <div className='w-10/12 mx-auto flex justify-center p-6 text-white text-3xl text-center googlefontpoppins'>Helping The New Moms Journey</div>
             <div className='w-10/12 mx-auto grid xxs:grid-cols-1 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-5 3xl:grid-cols-5 place-items-center gap-y-4 gap-x-4 mb-10 p-10'>
             <div className='flex flex-col items-center text-white gap-y-2 p-4'>
-              <div className='text-5xl font-extrabold text-orange-300'>38K+</div>
+              <div className='text-5xl font-extrabold text-orange-300'>39K+</div>
               <div className='text-2xl googlefontpoppins text-white'>Followers</div>
               <NavLink className='text-2xl underline googlefontpoppins text-white' to={'https://www.instagram.com/mommy_sanju/'} target="_blank">on Instagram</NavLink>
             </div>
