0d9de4a8fbe71f450738ebe817500557976a93b1
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-05-11 21:05:11 +0530

    play

--- src/components/InstaVideoCard.js
+++ src/components/InstaVideoCard.js
@@ -10,8 +10,8 @@ const InstaVideoCard = ({url, imageUrl, title}) => {
         <NavLink to={`${url}`} target='_blank' >
         <div className='relative w-[240px] bg-bgDark bg-opacity-80 rounded-lg overflow-hidden  pointer-events-auto'>
             <LazyLoadImage className=' h-[240px]' src={imageUrl} loading='lazy' alt='img'></LazyLoadImage>
-            <div className='absolute bottom-4 -right-2 h-8 w-[60px] font-semibold  text-white hover:scale-110 transition duration-300 ease-in'>
-                   <IoPlayCircleOutline size={50} />
+            <div className='absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                   <IoPlayCircleOutline size={100} />
               </div>
             </div>
             <p className='text-blue-700 font-bold text-xl leading-6 text-center mt-6'>{title}</p>
