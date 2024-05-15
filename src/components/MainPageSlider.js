a600b3c530ac7d89264ee09e1bd037e815b088af
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-05-15 11:18:09 +0530

    chnage

--- src/components/MainPageSlider.js
+++ src/components/MainPageSlider.js
@@ -54,8 +54,8 @@ const MainPageSlider = () => {
         {
             data.map((d) => (
                 <div className='w-full mx-auto'>
-                <div className='flex justify-evenly items-center py-4 '>
-                    <div className='flex flex-col justify-center w-6/12 mx-auto gap-y-10'>
+                <div className='flex justify-evenly p-3 '>
+                    <div className='flex flex-col justify-center w-5/12 mx-auto gap-y-10'>
                         <div className=' text-lg md:text-2xl poppins-medium'>
                             {d.title}
                         </div>
@@ -63,7 +63,7 @@ const MainPageSlider = () => {
                         <button className='bg-rose-600 px-1 md:px-4 py-1 md:py-2 rounded-lg text-white text-md font-semibold md:text-lg w-28 md:w-36 hover:bg-green-400 hover:text-black'>Click Here</button>
                         </Link>
                     </div>
-                    <div className='w-4/12 mx-auto'>
+                    <div className='w-5/12 mx-auto'>
                         <img src={d.imgUrl} alt='cover' className='h-40 md:h-56 rounded-lg'/>
                     </div>
                 </div>
